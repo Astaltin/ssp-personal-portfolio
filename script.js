3bf49d2f52819282cb9f51e1afccb78694cd2676
Astaltin <astaltin@gmail.com>
2024-03-21 10:17:49 +0800

    Initial commit

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-(async function () {
-	const contentView = document.body.querySelector('#contentView');
-
-	// Initializes the content view
-	if (contentView.childNodes.length === 0) {
-		setContentView('home');
-	}
-
-	addTabListener('#pills-home-tab', 'home');
-	addTabListener('#pills-projects-tab', 'projects');
-	addTabListener('#pills-about-tab', 'about');
-
-	function addTabListener(selector, page) {
-		document.body
-			.querySelector(selector)
-			.addEventListener('click', function () {
-				setContentView(page);
-			});
-	}
-
-	async function setContentView(page) {
-		const url = location.origin + `/pages/${page}.html`;
-		const response = await fetch(url);
-		contentView.innerHTML = await response.text();
-	}
-})();
+function projectsTab() {
+	document.body.querySelector('#projects-tab').click();
+}
